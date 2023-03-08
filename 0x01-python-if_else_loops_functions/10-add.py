b66c914fa5df24786c902251c27ab6ab6f1aa292
KanJosh <kanmodijoshua@gmail.com>
2023-03-08 14:48:57 +0100

    task10

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-def print_last_digit(number):
-    if number >= 0:
-        ld = number % 10
-    else:
-        ld = number % -10
-        ld *= -1
+# Author - Joshua Kanmodi
 
-    print("{:d}".format(ld), end='')
-    return (ld)
+def add(a, b):
+	"""Return the addition of a and b."""
+    return (a + b)
